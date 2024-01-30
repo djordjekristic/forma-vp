03c6ac7bce9a8df639fbc42ec5c6173962f2fee1
djordjekristic <strelac.989@gmail.com>
2024-01-30 14:41:44 +0100

    complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Places from "./Components/Places";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App(){
 
   return(
-    <>
+    <div className="d-flex justify-content-center container border border-4">
+      
     <Places/>
-    </>
+    </div>
     
   )
 
--- src/Components/Places.js
+++ src/Components/Places.js
@@ -13,30 +13,45 @@ function Places(){
     let[newPlace,setNewPlace]= useState("");
     let[newTemp,setNewTemp]= useState("");
     function newInfo(){
-        let newData= {newPlace:newTemp};
+        let newData= {[newPlace]:newTemp};
          if(newPlace===""){
-            return
+            return(alert('entry name'));
          }
          if(newTemp===""){
-            return
+            return(alert('entry temperature'));
          }
          setPlaces(currentPlaces=>({
             ...currentPlaces,
             ...newData
          }))
     }
+    function deleteTemp(e){
+        setPlaces("");
+
+    }
 
 
     return(
-        <>
-        <input onInput={(e)=>setNewPlace(e.target.value)} type="text"placeholder="Town name"/><br/>
-        <input onInput={(e)=>setNewTemp(e.target.value)} type="text"placeholder="Temperature"/>
-        <button onClick={newInfo}>Make</button>
-        {Object.entries(places).map(([place,temp])=>(
-            <p>{place}:{temp}C</p>
-        ))}
+        <div>
+            <h3 className="text-center text-muted mb-3">Temperature info</h3>
+            <div className="text-center">
+               
+        <input className="border border-success rounded" onInput={(e)=>setNewPlace(e.target.value)} type="text"placeholder="Town name"/><br/>
+                
+        <input className="border border-success rounded" onInput={(e)=>setNewTemp(e.target.value)} type="text"placeholder="Temperature"/>
+            </div>
+            <div className="m-2">
+        <button className=" btn btn-primary ms-2" onClick={newInfo}>Make</button>
+        <button className="btn btn-warning ms-2" onClick={deleteTemp}>Reset</button>
+           </div>
+        <div className=" text-center border border-3 border-info mb-4 mt-5">
+           {Object.entries(places).map(([place,temp])=>(
+            <p>{place} : {temp}C</p>
+           ))}
+        </div>
+        
 
-        </>
+        </div>
         
     )
 
